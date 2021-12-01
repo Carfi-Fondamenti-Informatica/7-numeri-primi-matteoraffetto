9499c556be4dcac46c1b764abffaaf394c959677
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-01 16:37:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+//
+// Created by matte on 01/12/2021.
+//
 
+#include "lib.h"
+
+bool primi (long &n){
+    bool flag = false;
+    if((n!=1)&(n!=2)){
+        for(long i=n-1;(i<n)&(i>1);i--){
+            if((n%i)==0){
+                flag=false;
+                break;
+            }else{
+                flag=true;
+            }
+        }
+    }else{
+        flag=true;
+    }
+    return flag;
+}
